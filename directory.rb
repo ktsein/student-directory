2133ba1e1e1d15ea6faeaa0e1571653ee5689aa3
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-09-18 16:47:20 +0000

    Did exercise 7

--- directory.rb
+++ directory.rb
@@ -1,17 +1,3 @@
-# let's put all the students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},  
-  {name: "Norman Bates", cohort: :november}
-]
 
 def print_header
 puts "The students of Villains Academy"
@@ -19,8 +5,8 @@ puts "------------"
 end
 
 def print(students)
-students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)}"
+students.each.with_index(1) do |student, index|
+    puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)}"
 end
 end
 
@@ -28,4 +14,24 @@ def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
 
+def input_students
+    
+    students = []
+    puts "Please enter the names of the students"
+    puts "To exit, just hit enter twice"
+    name = gets.chomp
+    puts "What is your cohort?"
+    cohort = gets.chomp
+    while !name.empty? && !cohort.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end
+    
+    students
+end
+
+students = input_students
+print_header
 print(students)
+print_footer(students)
