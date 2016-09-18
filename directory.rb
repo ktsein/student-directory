2e88d4cabce2e3c5af04d382e93aa432227e35f4
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-09-18 18:05:34 +0000

    Fixed errors in exercises 8

--- directory.rb
+++ directory.rb
@@ -1,35 +1,37 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+  # add the student hash to the array
+  students << {name: name , cohort: :november}
+  puts "Now we have #{students.count} students"
+  # get another name from the user
+  name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
-puts "The students of Villains Academy"
-puts "------------"
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
-def print(students)
-students.each.with_index(1) do |student, index|
-    puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)}"
-end
-end
-
-def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+def students
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
-def input_students
-    
-    students = []
-    puts "Please enter the names of the students"
-    puts "To exit, just hit enter twice"
-    name = gets.chomp
-    puts "What is your cohort?"
-    cohort = gets.chomp
-    while !name.empty? && !cohort.empty? do
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
-        name = gets.chomp
-    end
-    
-    students
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
 end
+
 
 students = input_students
 print_header
