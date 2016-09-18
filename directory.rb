8fd90ba0ed56b61eaf13e019ad19b435e01a6471
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-09-18 20:06:30 +0000

    Add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,37 @@
+@students = []
+
+def process(selection)
+        case selection
+        when "1"
+            input_students
+        when "2"
+            show_students
+        when "9"
+            exit
+        else
+            puts "I don't know what you meant, try again"
+    end
+end
+
+def interactive_menu
+    loop do
+        print_menu
+        process(gets.chomp)
+    end
+end
+
+def print_menu
+        puts "1. Input the students"
+        puts "2. Show the students"
+        puts "9. Exit"
+    end
+    
+def show_students
+    print_header
+    print_students_list
+    print_footer(student)
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -8,7 +42,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
   # add the student hash to the array
-  students << {name: name , cohort: :november}
+  students << {name: name, cohort: :november}
   puts "Now we have #{students.count} students"
   # get another name from the user
   name = gets.chomp
@@ -22,18 +56,18 @@ def print_header
   puts "-------------"
 end
 
-def students
+def print_students_list
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{students.count} great students"
 end
 
 
 students = input_students
 print_header
-print(students)
-print_footer(students)
+print_students_list
+print_footer
